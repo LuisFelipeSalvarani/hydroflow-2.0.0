909d7f2626564ff821d55b8ea8a9d4f37858f004
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-07-26 19:38:46 -0300

    Atualizando usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,7 @@ const User = require("../models/User")
 
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const mongoose = require("mongoose")
 
 const jwsSecret = process.env.JWT_SECRET
 
@@ -80,7 +81,27 @@ const getCurrentUser = async (req, res) => {
 
 // Atualizar usuário
 const update = async (req, res) => {
-    res.send("update")
+    const { password, isAdmin } = req.body
+
+    const reqUser = req.user
+
+    const user = await User.findById(new mongoose.Types.ObjectId(reqUser._id)).select("-password")
+
+    if(password) {
+        // Geração do Hash da senha
+        const salt = await bcrypt.genSalt()
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        user.password = passwordHash
+    }
+
+    if(isAdmin) {
+        user.isAdmin = isAdmin
+    }
+
+    await user.save()
+
+    res.status(200).json(user)
 }
 
 module.exports = {
