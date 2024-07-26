1647dea6da9cb7fbecdf817859c065aad75fa479
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-07-25 21:57:33 -0300

    Middleware de validação

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const router = express.Router()
 
+// Controller
 const { register } = require("../controllers/UserController")
 
-router.post("/register", register)
+// Middlewares
+const validate = require("../middlewares/handleValidation")
+
+// Routes
+router.post("/register", validate, register)
 
 module.exports = router
