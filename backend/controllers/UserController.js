de454780999b6ff1d1b0c3c39fe1b7d813d9de5c
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-07-25 23:07:05 -0300

    Registro de usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -11,8 +11,38 @@ const generateToken = (id) => {
     })
 }
 
+// Register user
 const register = async(req, res) => {
-    res.send("Registro")
+    const { username, password, isAdmin } = req.body
+
+    // Check if user exists
+    const user = await User.findOne({ username })
+
+    if(user) {
+        res.status(422).json({errors: ["Nome de usuário já existente."]})
+        return
+    }
+
+    // Generate password hash
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // Create user
+    const newUser = await User.create({
+        username,
+        password: passwordHash,
+        isAdmin
+    })
+
+    // If user was created successfully, return the token
+    if(!newUser) {
+        res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde."]})
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+    })
 }
 
 module.exports = {
