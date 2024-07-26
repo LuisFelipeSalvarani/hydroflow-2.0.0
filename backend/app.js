bd6c28be5740885f470df2c30c38bdecab0ff9ed
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-07-25 22:34:59 -0300

    Middleware de validação do usuário, validações do registro de usuários

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,18 @@ require("dotenv").config()
 
 const express = require("express")
 const path = require("path")
+const cors = require("cors")
 
 const port = process.env.PORT
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(cors({credentials: true, origin: "http://localhost:5173"}))
+
+// Conexão com DB
 require("./config/db")
 
 const router = require("./routes/Router")
---
+++ backend/middlewares/userValidation.js
@@ -0,0 +1,46 @@
+const { body } = require("express-validator")
+
+const userCreateValidation = () => {
+    return [
+        // Validação do nome de usuário
+        body("username")
+            .isString()
+            .withMessage("O nome de usuário é obrigatório.")
+            .isLength({ min: 3 })
+            .withMessage("O nome de usuário precisa ter no mínimo 3 caracteres."),
+
+        // Validação da senha
+        body("password")
+            .isString()
+            .withMessage("A senha é obrigatória.")
+            .isLength({ min: 8 })
+            .withMessage("A senha precisa ter no mínimo 8 caracteres.")
+            .custom(value => {
+                const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).+$/
+                if (!regex.test(value)) {
+                    throw new Error('A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.')
+                }
+                return true
+            }),
+
+        // Validação da confirmação de senha
+        body("confirmpassword")
+            .isString()
+            .withMessage("A confirmação de senha é obrigatória.")
+            .custom((value, { req }) => {
+                if (value !== req.body.password) {
+                    throw new Error("As senhas devem ser iguais.")
+                }
+                return true
+            }),
+        // Validação do campo isAdmin
+        body("isAdmin")
+            .optional()
+            .isBoolean()
+            .withMessage("O campo isAdmin deve ser um valor booleano."),
+    ]
+}
+
+module.exports = {
+    userCreateValidation,
+}
