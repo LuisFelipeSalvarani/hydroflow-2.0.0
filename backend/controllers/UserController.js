06ddd50870014acf659b8562a7c55399def30eb5
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-07-27 14:24:20 -0300

    Obtendo usuário pelo id

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -104,9 +104,30 @@ const update = async (req, res) => {
     res.status(200).json(user)
 }
 
+// Obter usuário pelo id
+const getUserById = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const user = await User.findById(new mongoose.Types.ObjectId(id)).select("-password")
+
+        // Checagem da existência do usuário
+        if(!user) {
+            res.status(404).json({errors: ["Usuário não encontrado."]})
+            return
+        }
+
+         res.status(200).json(user)
+    } catch (error) {
+        res.status(404).json({errors: ["Usuário não encontrado."]})
+        return
+    }
+}
+
 module.exports = {
     register,
     login,
     getCurrentUser,
     update,
+    getUserById,
 }
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 // Controller
-const { register, login, getCurrentUser, update } = require("../controllers/UserController")
+const { register, login, getCurrentUser, update, getUserById } = require("../controllers/UserController")
 
 // Middlewares
 const validate = require("../middlewares/handleValidation")
@@ -14,5 +14,6 @@ router.post("/register", userCreateValidation(), validate, register)
 router.post("/login", loginValidation(), validate, login)
 router.get("/profile", authGuard, getCurrentUser)
 router.put("/", authGuard, userUpdateValidation(), validate, update)
+router.get("/:id", authGuard, getUserById)
 
 module.exports = router
